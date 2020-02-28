2307da69666efdcec8f247fce7066944a2f109dd
hoangson763763 <hoangson763763@gmail.com>
2020-02-28 14:34:43 +0700

    bai1-expressjs

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/',(request,response) => response.send('Hello word'));
+app.listen(port,() => console.log(`Example app listening on port ${port}`));
